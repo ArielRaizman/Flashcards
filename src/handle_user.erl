d6f8be2bc6bc9daead390c5b3bedfa083b77aa4a
DaBlob <ariel.raizman@hotmail.com>
2015-03-15 21:53:49 -0700

    Implement 1st "elephant" state machine.

--- src/handle_user.erl
+++ src/handle_user.erl
@@ -20,14 +20,44 @@ terminate(_Reason, _Req, _State) ->
 	ok.
 
 handle_get(Req) ->
+	W = 0,
+	C = 0,
 	reply_dtl(Req, user_dtl, [
 		% parameters like {card, Value}
-		]).
+		{wrong, W},
+		{correct, C},
+		{card, "B"}
+	]).
 
 handle_post(Req) ->
-	reply_dtl(Req, user_dtl, [
+	{ok, KeyVals, Req2} = cowboy_req:body_qs(Req, []),
+	{C, W} = get_status(KeyVals),
+	{AddC, AddW} = get_res(KeyVals),
+	NewW = W + AddW,
+	NewC = C + AddC,
+	reply_dtl(Req2, user_dtl, [
 		% parameters like {card, Value}
-		]).
+		{wrong, NewW},
+		{correct, NewC},
+		{card, "B"}
+	]).
+
+get_status(KeyVals) ->
+	get_status(
+		lists:keyfind(<<"c">>, 1, KeyVals),
+		lists:keyfind(<<"w">>, 1, KeyVals)).
+get_status({_, C},{_, W}) -> {to_num(C), to_num(W)}.
+
+get_res(KeyVals) ->
+	get_res(
+		lists:keyfind(<<"b">>, 1, KeyVals),
+		lists:keyfind(<<"d">>, 1, KeyVals)).
+get_res(false, {_, _D}) -> {0, 1};
+get_res({_, _B}, false) -> {1, 0}.
+
+to_num(B) when is_binary(B) -> to_num(binary_to_list(B));
+to_num(L) when is_list(L)   -> list_to_integer(L);
+to_num(N)                   -> N.
 
 
 reply_dtl(Req, Template, Params) ->
